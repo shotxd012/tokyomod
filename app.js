a02ea7aaa5224daf4c7d35a9ab636cfffb2f5f7b
shotxd012 <shotxd506@gmail.com>
2025-01-10 22:46:56 +0530

    v2 app.js

--- app.js
+++ app.js
@@ -8,8 +8,15 @@ app.set('view engine', 'ejs');
 
 app.use(express.static('public'));
 
+const getFiles = (dirPath) => {
+  return fs.readdirSync(dirPath).map(file => ({
+    name: file,
+    size: (fs.statSync(path.join(dirPath, file)).size / 1024).toFixed(2) + ' KB',
+  }));
+};
+
 app.get('/', (req, res) => {
-  const dirPath = path.join(__dirname, 'public', 'mod');
+  const dirPath = path.join(__dirname, 'public', 'mod', 'new');
   const files = fs.readdirSync(dirPath).map(file => ({
     name: file,
     size: (fs.statSync(path.join(dirPath, file)).size / 1024).toFixed(2) + ' KB',
@@ -17,28 +24,47 @@ app.get('/', (req, res) => {
   res.render('index', { files });
 });
 
-app.get('/download-all', (req, res) => {
-  const zip = new AdmZip();
+app.get('/mods', (req, res) => {
   const dirPath = path.join(__dirname, 'public', 'mod');
+  const files = getFiles(dirPath);
+  res.render('download', { files, type: 'mods', title: 'Download All Mods' });
+});
 
+app.get('/mods/new', (req, res) => {
+  const dirPath = path.join(__dirname, 'public', 'mod', 'new');
+  const files = getFiles(dirPath);
+  res.render('download', { files, type: 'new-mods', title: 'New Upload Mods' });
+});
+
+app.get('/download-all/:type', (req, res) => {
+  const { type } = req.params;
+  const dirPath = type === 'new-mods'
+    ? path.join(__dirname, 'public', 'mod', 'new')
+    : path.join(__dirname, 'public', 'mod');
+
+  const zip = new AdmZip();
   fs.readdirSync(dirPath).forEach(file => {
     zip.addLocalFile(path.join(dirPath, file));
   });
 
-  const zipPath = path.join(__dirname, 'public', 'mod.zip');
+  const zipPath = path.join(__dirname, `public/${type}-mods.zip`);
   zip.writeZip(zipPath);
 
-  res.download(zipPath, 'TokyoMC-Mods.zip', err => {
+  res.download(zipPath, `${type}-mods.zip`, err => {
     if (err) {
       console.error(err);
     }
-    fs.unlinkSync(zipPath); 
+    fs.unlinkSync(zipPath);
   });
 });
 
-app.get('/download/:fileName', (req, res) => {
-  const fileName = req.params.fileName;
-  const filePath = path.join(__dirname, 'public', 'mod', fileName);
+app.get('/download/:type/:fileName', (req, res) => {
+  const { type, fileName } = req.params;
+  const dirPath = type === 'new-mods'
+    ? path.join(__dirname, 'public', 'mod', 'new')
+    : path.join(__dirname, 'public', 'mod');
+
+  const filePath = path.join(dirPath, fileName);
 
   if (fs.existsSync(filePath)) {
     res.download(filePath, fileName, err => {
@@ -53,7 +79,11 @@ app.get('/download/:fileName', (req, res) => {
 });
 
 
+app.get('/store', (req, res) => {
+  res.render('store');
+});
+
 const PORT = 3000;
 app.listen(PORT, () => {
-  console.log(`Server is running on http://mods.tokyomc.fun`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
